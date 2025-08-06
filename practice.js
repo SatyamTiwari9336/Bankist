a796e59d7bd5ff7b75628b9c911f0125ef6aaa6f
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-08-06 16:05:42 +0530

    maximum element of an array

--- practice.js
+++ practice.js
@@ -1,1 +1,12 @@
 const array = [2, 4, 7, 6, 1];
+//to get the maximum element in the array
+const max = function (array) {
+  let currmax = 0;
+  for (const cur of array) {
+    if (cur > currmax) {
+      currmax = cur;
+    }
+  }
+  console.log(`max is ${currmax}`);
+};
+max(array);
