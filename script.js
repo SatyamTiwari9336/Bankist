715c8421c8ac6177a525b604e424064ef97dca7f
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-08-13 17:44:41 +0530

    new method

--- script.js
+++ script.js
@@ -484,8 +484,8 @@ console.log(movements.filter(deposits));
 let arr = [1, 2, 3, [2, 3, 4]];
 console.log(Array.isArray(arr[3]));
 console.log(Array.isArray(arr[2]));
-*/
-//array flatning
+
+//array flatening
 const arr = [[1, 2, 3], [4, 5, 3], 7, 8];
 console.log(arr.flat());
 const arrDeep = [[1, [3, 2, [3482], 8], 3], [4, [2, 6, 7], 5, 3], [7, 2, 5], 8];
@@ -493,3 +493,76 @@ console.log(arrDeep.flat(2));
 console.log(arrDeep.flat(3));
 const arrnew = [1, 2, 3, [4, 3, [1, 2, 3]]];
 console.log(arrnew.flat(5));
+
+const overallbalance = accounts
+  .map(mov => mov.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(overallbalance);
+
+//flatMap does same thing as map and then flats the results
+
+const overallbalance2 = accounts
+  .flatMap(mov => mov.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(overallbalance);
+
+*/
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+
+const breeds = [
+  {
+    breed: 'German Shepherd',
+    averageWeight: 32,
+    activities: ['fetch', 'swimming'],
+  },
+  {
+    breed: 'Dalmatian',
+    averageWeight: 24,
+    activities: ['running', 'fetch', 'agility'],
+  },
+  {
+    breed: 'Labrador',
+    averageWeight: 28,
+    activities: ['swimming', 'fetch'],
+  },
+  {
+    breed: 'Beagle',
+    averageWeight: 12,
+    activities: ['digging', 'fetch'],
+  },
+  {
+    breed: 'Husky',
+    averageWeight: 26,
+    activities: ['running', 'agility', 'swimming'],
+  },
+  {
+    breed: 'Bulldog',
+    averageWeight: 36,
+    activities: ['sleeping'],
+  },
+  {
+    breed: 'Poodle',
+    averageWeight: 18,
+    activities: ['agility', 'fetch'],
+  },
+]; */
