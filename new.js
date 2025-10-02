f8c0981bd72601bd15c754404ee8910f9995bc98
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-02 16:38:57 +0530

    palindrome check

--- new.js
+++ new.js
@@ -1,9 +1,23 @@
-function isPalindromeNumber(num) {
+const ispalindrome = function (num) {
   const str = num.toString();
   const reversed = str.split("").reverse().join("");
-  return str === reversed;
+  console.log(reversed);
+  console.log(reversed == str);
+};
+
+ispalindrome(112);
+ispalindrome(121);
+
+function reverseNumber(num) {
+  let newnum = 0;
+  let reverse = 0;
+  while (num > 0) {
+    newnum = num % 10;
+    reverse += newnum.toString();
+    num = Math.floor(num / 10);
+  }
+  const reversenum = Number(reverse);
+  console.log(reversenum);
 }
 
-// Example usage:
-console.log(isPalindromeNumber(121)); // true
-console.log(isPalindromeNumber(123)); // false
+reverseNumber(13289);
